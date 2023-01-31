e65b64f2dc8dea0dfb575a9bc677678b63950cb9
MarcoMaddo <profmaddo@gmail.com>
2023-01-31 12:11:08 -0400

    Primeiro Commit

---
+++ QuintoPrograma/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val cidade: String = "Paris"
+    val nomeCompleto: String = "João José"
+    val precoHospedagem: Double = 123.45
+
+    // Contatenação de textos = Strings com atributos declados
+
+    println("Cidade $cidade Nome do Hóspede $nomeCompleto Valor da Diária $precoHospedagem")
+
+}
