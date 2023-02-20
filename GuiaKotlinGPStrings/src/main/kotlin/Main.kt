b9ad7f46bc5c311fbf14e3dfed548de22df78d8a
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-20 11:39:58 -0400

    Kotlin GuiaGP - Strings

---
+++ GuiaKotlinGPStrings/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    // Strings
+    // \t \b \n \r \\ \$
+    // Contatenar + Strings
+    // Raw Strings
+
+
+    val preco : String = "R$ 12.99"
+    var Kotlin =
+        """  ---
+                     KOTLIN CURSO $preco
+           DE
+       Programação *** \n--- 2023"""
+
+    val texto : String = "Formação " + Kotlin + " Total - Guia de Programação"
+
+    println(texto)
+
+}
