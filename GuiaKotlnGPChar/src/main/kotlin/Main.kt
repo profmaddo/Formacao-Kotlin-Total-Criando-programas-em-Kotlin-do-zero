18d5b98e5120fffaeaa2aeda8e8bc204244275f4
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-20 11:22:29 -0400

    Kotlin GuiaGP - Char unicode

---
+++ GuiaKotlnGPChar/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    // Char - Caracteres Literais - aspas simples
+    // codificação Unicode
+    // CR ENTER - U+01A9 - Ʃ
+
+    var caractereUnicode : Char = '\u01A9'
+
+    println("Unicode U+01A2: $caractereUnicode")
+}
